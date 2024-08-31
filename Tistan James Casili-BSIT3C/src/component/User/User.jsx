a1ac4747fd79efd880c9e29727d0d4e6e5549b9b
Tristan James F. Casili <151809262+Tristan1820-oop@users.noreply.github.com>
2024-08-31 17:13:13 +0800

    Add files via upload
    thank you po sir

---
+++ Tistan James Casili-BSIT3C/src/component/User/User.jsx
@@ -0,0 +1,12 @@
+function User() {
+    return (
+        <div>
+        <h1>    Tristan James F. Casili   </h1>
+        <h2>        BSIT_3B               </h2>
+        <p>       My Best Work             </p>
+     </div>
+
+    )
+}
+
+export default User;
