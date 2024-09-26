9513976fed0447cc05af33c15506ce79aba247c1
Tristan James F. Casili <151809262+Tristan1820-oop@users.noreply.github.com>
2024-09-27 00:00:49 +0800

    Hands-on1_ACT1
    activity ko po sir ito po yung papacheck ko po thank you po

---
+++ Hands-on1/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1> Tristan James F. Casili </h1>
+      <h2> BSIT_3C </h2>
+      <p> PAGKAKALAMEEEG MAG CODE </p>
+    </div>
+  );
+}
+
+export default App;
